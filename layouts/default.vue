61dbcca1c2481dea90287b56ace2802c8c1e2637
WebcoderPartha <parthadev76@gmail.com>
2023-06-29 00:36:02 +0600

    Header and footer done

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <Header />
+        <slot />
+        <Footer />
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,10 @@
 <template>
     <div>
         <h2 class="text-xs text-blue-300">Home page</h2>
+        <h2 class="text-xs text-blue-300">Home page</h2>
+        <h2 class="text-xs text-blue-300">Home page</h2>
+        <h2 class="text-xs text-blue-300">Home page</h2>
+        
     </div>
 </template>
 
