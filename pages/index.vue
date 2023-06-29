087fb7e39b704a10974e7310172f0b0c48a5e776
WebcoderPartha <parthadev76@gmail.com>
2023-06-29 23:03:33 +0600

    Todo store

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,13 @@
 <template>
     <div class="h-[600px] bg-gray-700">
         <section class="p-6 dark:text-gray-100">
-		<form novalidate="" class="container w-full max-w-xl p-8 mx-auto space-y-6 rounded-md shadow dark:bg-gray-900">
+		<form @submit.prevent="todoHandle" novalidate="" class="container w-full max-w-xl p-8 mx-auto space-y-6 rounded-md shadow dark:bg-gray-900">
 			<h2 class="w-full text-3xl font-bold leadi text-center">Add Todo</h2>
 			<div>
 				<label for="name" class="block mb-1 ml-1">Write todo</label>
-				<input id="name" type="text" placeholder="Type your text" required="" class="block w-full p-2 rounded focus:outline-none focus:ring focus:ri focus:ri dark:bg-gray-800">
+				<input id="name" v-model="todo.todo" type="text" placeholder="Type your text" required="" class="block w-full p-2 rounded focus:outline-none focus:ring focus:ri focus:ri dark:bg-gray-800">
+                <span class="text-red-900">{{ error.error }}</span>
+                {{ todo.todo }}
 			</div>
 			<!-- <div>
 				<label for="email" class="block mb-1 ml-1">Email</label>
@@ -16,7 +18,8 @@
 				<textarea id="message" type="text" placeholder="Message..." class="block w-full p-2 rounded autoexpand focus:outline-none focus:ring focus:ri focus:ri dark:bg-gray-800"></textarea>
 			</div> -->
 			<div>
-				<button type="submit" class="w-full px-4 py-2 font-bold rounded shadow focus:outline-none focus:ring hover:ring focus:ri dark:bg-violet-400 focus:ri hover:ri dark:text-gray-900">Add Todo</button>
+				<button v-if="isPending" disabled type="submit" class="w-full px-4 py-2 font-bold rounded shadow focus:outline-none focus:ring hover:ring focus:ri dark:bg-violet-400 focus:ri hover:ri dark:text-gray-900">Adding Todo</button>
+				<button v-else type="submit" class="w-full px-4 py-2 font-bold rounded shadow focus:outline-none focus:ring hover:ring focus:ri dark:bg-violet-400 focus:ri hover:ri dark:text-gray-900">Add Todo</button>
 			</div>
 		</form>
 	</section>
@@ -25,6 +28,49 @@
 </template>
 
 <script setup>
+    // Site meta add
+    useHead({
+        title: 'Todo App'
+    })
+
+    const todo = useState(() => ({
+        todo: ''
+    }))
+
+    const error = useState(() => ({
+        error: ''
+    }))
+    
+    const isPending = false
+
+    const todoHandle = async (e) => {
+        const isPending = true
+
+        if(todo.value.todo.length === 0) {
+            error.value.error = 'Field must not be empty!'
+        }else{
+            
+          
+            const todoData = {
+                todo: todo.value.todo,
+                status: true
+            }
+            const {data} = await useFetch('http://localhost:5000/todo', {
+                method:'POST',
+                body: JSON.stringify(todoData),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+
+            const isPending = false
+            error.value.error = '',
+            todo.value.todo = ''
+            e.target.reset();
+
+        }
+    }
+
 
 </script>
 
