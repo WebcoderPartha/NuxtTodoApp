556d7666ed7b3971185f716efd288094daf72691
WebcoderPartha <parthadev76@gmail.com>
2023-06-30 11:59:24 +0600

    Todo list design

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-[600px] bg-gray-700">
+  <div class="min-h-[600px] bg-gray-700 pb-5">
     <section class="p-6 dark:text-gray-100">
       <form
         @submit.prevent="todoHandle"
@@ -41,10 +41,30 @@
     </section>
 
     <!-- Todo List -->
-    <div v-if="todolist.length > 0" class=" w-full max-w-xl p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-900 dark:border-gray-700 mx-auto">
-        <h3 class="w-full text-3xl font-bold leadi text-center text-white border-b">Todo List</h3>
+    <div v-if="todolist.length > 0" class=" w-full max-w-[800px] p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-900 dark:border-gray-700 mx-auto">
+        <h3 class="w-full text-3xl font-bold leadi text-center text-white border-b mb-5">Todo List</h3>
  
-        <h5 v-for="todo in todolist" class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ todo.todo }}</h5>
+      <div  v-for="todo in todolist" class=" bg-sky-900 py-2 px-5 text-center mb-2 rounded-2xl">
+        <div class="grid grid-cols-4 gap-4">
+          <div class="col-span-3">
+            <h5 v-if="todo.status" class="mb-2 text-2xl tracking-tight text-white">{{ todo.todo }}</h5>
+            <h5 v-else class="mb-2 text-2xl tracking-tight text-red-700 line-through">{{ todo.todo }}</h5>
+          </div>
+          <div>
+            <button class="text-fuchsia-950">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+              </svg>
+            </button>
+            &nbsp;
+            <button class="text-red-900">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
   
     </div>
 
